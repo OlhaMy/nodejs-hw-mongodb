f41a83ab3b09b67ade6a18635c087cac0fc68114
Olha Mykhalchuk <124881449+OlhaMy@users.noreply.github.com>
2024-10-31 23:00:28 +0100

    added reset password

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,12 +1,17 @@
 import { Router } from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
-import { registerUserSchema, loginUserSchema } from '../validation/auth.js';
+import {
+  registerUserSchema,
+  loginUserSchema,
+  requestResetEmailSchema,
+} from '../validation/auth.js';
 import { validateBody } from '../utils/validateBody.js';
 import {
   registerUserController,
   loginUserController,
   logoutUserController,
   refreshUserSessionController,
+  requestResetEmailController,
 } from '../controllers/auth.js';
 
 const routerAuth = Router();
@@ -26,4 +31,9 @@ routerAuth.post(
 routerAuth.post('/logout', ctrlWrapper(logoutUserController));
 routerAuth.post('/refresh', ctrlWrapper(refreshUserSessionController));
 
+routerAuth.post(
+  '/send-reset-email',
+  validateBody(requestResetEmailSchema),
+  ctrlWrapper(requestResetEmailController),
+);
 export default routerAuth;
